a85f34c7236735d46af230fe04e5d0c46179985b
GitHubBruger <ianb@kea.dk>
2024-03-18 11:30:46 +0100

    Added dev and prod profiles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,16 @@
-Insert into DEPT (DEPTNO,DNAME,LOC) values (10,'ACCOUNTINGX','NEW YORK');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (20,'RESEARCHX','DALLAS');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (30,'SALESX','CHICAGO');
-Insert into DEPT (DEPTNO,DNAME,LOC) values (40,'OPERATIONSX','BOSTON');
+CREATE SCHEMA IF NOT EXISTS EMP_DEPT;
+
+DROP TABLE IF EXISTS EMP_DEPT.DEPT;
+
+CREATE TABLE EMP_DEPT.DEPT (
+                      DEPTNO INTEGER,
+                      DNAME VARCHAR(30),
+                      LOC VARCHAR(30),
+                      PRIMARY KEY (DEPTNO)
+);
+
+INSERT INTO EMP_DEPT.DEPT (DEPTNO, DNAME, LOC) VALUES
+                                          (10, 'ACCOUNTING_H2', 'NEW YORK'),
+                                          (20, 'RESEARCH_H2', 'DALLAS'),
+                                          (30, 'SALES_H2', 'CHICAGO'),
+                                          (40, 'OPERATIONS_H2', 'BOSTON');
